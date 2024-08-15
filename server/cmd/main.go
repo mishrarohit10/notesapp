5897d11e8dd833e538d9e6a39bed05ab67260a78
rohit <mishrarohit316x@gmail.com>
2024-08-16 01:12:22 +0530

    added rootHandler

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"notesapp/server/internal/user"
 
 	"github.com/gin-gonic/gin"
@@ -12,11 +13,17 @@ func main() {
 
 	router := gin.Default()
 
+	router.GET("/", rootHandler)
+
 	setupRoutes(router)
 
 	if err := router.Run(); err != nil {
 		panic(err)
 	}
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "working fine"})
 }
 
 func setupRoutes(router *gin.Engine) {
@@ -25,7 +32,6 @@ func setupRoutes(router *gin.Engine) {
 
 func userRouter(router *gin.Engine) {
 	userGroup := router.Group("/")
-
 	{
 		userGroup.POST("/login", user.Login)
 	}

6879c68e339634929fc3f9160e2bacd992e9fb41
rohit <mishrarohit316x@gmail.com>
2024-08-19 01:13:56 +0530

    added db creds from .env file and error handling

--- server/internal/db/connection.go
+++ server/internal/db/connection.go
@@ -3,21 +3,38 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func Init() error {
-	
-	connStr := fmt.Sprintf("host=localhost port=5432 user=postgres password=%s dbname=postgres sslmode=disable", os.Getenv("POSTGRES_PASSWORD"))
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	var err error
 	DB, err = sql.Open("postgres", connStr)
 
 	if err != nil {
+		log.Fatalf("Error opening database connection: %v", err)
 		return err
 	}
 
-	return DB.Ping()
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf("Error pining database: %v", err)
+		return err
+	}
+
+	log.Println("Database connection succesfull")
+	return nil
 }

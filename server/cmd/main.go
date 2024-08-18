2f8cd276839f55601386716968707411c0de66fb
rohit <mishrarohit316x@gmail.com>
2024-08-19 01:12:39 +0530

    added db creds from env file

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"notesapp/server/internal/db"
 	"notesapp/server/internal/user"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hi from the server")
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Error getting current working directory: %v", err)
+	}
+
+	log.Println("Current working directory ->", cwd)
+
+	envPath := filepath.Join(cwd, "../../server/.env")
+
+	log.Println("Environment variable path ->",envPath)
+
+	if _, err := os.Stat(envPath); os.IsNotExist(err) {
+		log.Fatalf(".env file does not exist at path: %s", envPath)
+	}
+
+	err = godotenv.Load(envPath)
+	if err != nil {
+		log.Fatalf("Error while loading .env file")
+	}
+
+	err = db.Init()
+	if err != nil {
+		log.Fatalf("Failed to connect to Database")
+	}
+
+	log.Println("Database connect succesfull")
 
 	router := gin.Default()
 
@@ -34,5 +64,6 @@ func userRouter(router *gin.Engine) {
 	userGroup := router.Group("/")
 	{
 		userGroup.POST("/login", user.Login)
+		userGroup.POST("/signup", user.SignUp)
 	}
 }

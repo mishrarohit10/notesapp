07931db33242c54ca4c615777a7bfa8849669cad
rohit <mishrarohit316x@gmail.com>
2024-08-20 01:28:02 +0530

    updated signup and login connected to db

--- server/internal/user/handler.go
+++ server/internal/user/handler.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"net/http"
+	"notesapp/server/internal/db"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,7 +10,7 @@ import (
 func Login(c *gin.Context) {
 
 	type Login struct {
-		Username string `json:"username" binding:"required"`
+		Email    string `json:"email" binding:"required,email"`
 		Password string `json:"password" binding:"required"`
 	}
 
@@ -19,5 +20,61 @@ func Login(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Logged In", "email": creds.Username, "password": creds.Password})
+	// check if email is present in db or not
+	var exists bool
+	err := db.DB.Debug().Raw("SELECT EXISTS (SELECT 1 FROM users WHERE email=?)", creds.Email).Scan(&exists).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "This email has no account"})
+		return
+	}
+
+	var storedPassword string
+	err = db.DB.Debug().Raw("SELECT password FROM users WHERE email=?", creds.Email).Scan(&storedPassword).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Password not found"})
+		return
+	}
+
+	// check if password is correct or not
+	if creds.Password != storedPassword {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "Incorrect password"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Logged In", "error": false})
+}
+
+func SignUp(c *gin.Context) {
+
+	type SignUp struct {
+		Email    string `json:"email" binding:"required,email"`
+		Password string `json:"password" binding:"required"`
+	}
+
+	var creds SignUp
+	if err := c.BindJSON(&creds); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "error bindingJSON", "error": true})
+		return
+	}
+
+	// check if email is present in db or not
+	var exists bool
+	err := db.DB.Debug().Raw("SELECT EXISTS (SELECT 1 FROM users WHERE email=?)", creds.Email).Scan(&exists).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Error querying email database"})
+		return
+	}
+
+	if exists {
+		c.JSON(http.StatusConflict, gin.H{"message": "Email already exists"})
+		return
+	}
+
+	err = db.DB.Debug().Exec("INSERT INTO users (email, password) VALUES (?,?)", creds.Email, creds.Password).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to save to database"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Signed Up", "email": creds.Email, "error": false})
 }
